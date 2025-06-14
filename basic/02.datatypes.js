27486c12e8715a21fb1e2d19a2c18dbe4543de09
aryan-novice <aryanabhi991@gmail.com>
2025-06-14 20:41:04 +0000

    datatypes

---
+++ basic/02.datatypes.js
@@ -0,0 +1,12 @@
+let name = "Aryan";
+let age = 14;
+let isLoggedin =  true;
+
+console.log(name)
+console.log(typeof age);
+
+let state;
+console.log(typeof state);
+
+let temp = null;   //null datatype is a object
+console.log(typeof temp);
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,19 @@
-console.log("Aryan")
+const accountId =  3242323
+let accountEmail = "aryan@gmai.com"
+var accoutPassword = 324235
+accountcity = "Jaipur"
+
+//variable without value
+let accountState;
+
+// accountId = 3927  //it will show error
+// console.log(accountId)
+
+accountEmail = "aryanjseefob"
+accountPassword = 3287
+accountcity = "Ajmer"
+
+console.log([accountEmail,accountPassword,accountcity,accountState])
+
+    
+console.table([accountEmail,accountPassword,accountcity,accountState])

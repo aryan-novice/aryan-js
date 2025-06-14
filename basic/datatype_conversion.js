f4ddc3ffb27db685115fd0288de9ec08f7adcf68
aryan-novice <aryanabhi991@gmail.com>
2025-06-14 21:08:44 +0000

    datatype_conversion

---
+++ basic/datatype_conversion.js
@@ -0,0 +1,18 @@
+// let score = 33;
+// let score = "33";
+// let score = 33abs;
+// let score = true;
+// let score = false;
+// let score = null;
+// let score;
+let score = "Aryan";
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+ 

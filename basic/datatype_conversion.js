fd6662ef1e7d476071d4773d66e8d88b4ddee8fe
aryan-novice <aryanabhi991@gmail.com>
2025-06-16 19:46:51 +0000

    Extended_conversion

--- basic/datatype_conversion.js
+++ basic/datatype_conversion.js
@@ -1,11 +1,11 @@
 // let score = 33;
 // let score = "33";
 // let score = 33abs;
-// let score = true;
+// let score = tlrue;
 // let score = false;
 // let score = null;
 // let score;
-let score = "Aryan";
+// let score = "Aryan";
 
 
 
@@ -16,3 +16,54 @@ let score = "Aryan";
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
  
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "";
+// let isLoggedIn = "Aryan";
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let someno = 33;
+
+// let stringno = String(someno);
+// console.log(stringno);
+// console.log(typeof stringno);
+
+
+//*****************opertions */
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = "Aryan";
+
+// let str3 =  (str1+str2);
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+4+6);
+// // console.log(1+2-1+"1");
+
+
+
+// let gamecounter =  100;
+// gamecounter++;
+// console.log(gamecounter);
+
+
+
+
+
+

114173539e28e2aa7f9b7bc399e49c2e476ba478
aryan-novice <aryanabhi991@gmail.com>
2025-06-16 20:39:21 +0000

    datatype_summmary

---
+++ basic/datatype_summary.js
@@ -0,0 +1,28 @@
+const score = 100;
+const scores =  1000.3;
+
+const isLoggedIn = true;
+const temperature = null;
+let userName; //undefined
+
+let userAge = undefined;
+
+//Symabol is a unique and immutable data type
+const sym1 = Symbol('123');
+const sym2 = Symbol('123');
+console.log(sym1===sym2);
+
+
+//Arrays
+const arr = [1, 2, 3, 4, 5];
+
+//Objects
+const user = {
+    name:"Aryan",
+    age :23
+}
+
+//Functions
+const myfunction = function() {
+    console.log("Aryan");
+}
